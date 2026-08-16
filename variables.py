666e2c596377f68d9b8371ed06eb7a0856ef068f
Arun <appanaboyanaarun@gmail.com>
2026-08-16 13:18:59 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+#In Python ,The Variables are used to store different values 
+#Python does not require you to specify the data type when creating a variable.
+''' Example:
+ age =20 #correct
+ int age =20 #incorrect
+ '''
+
+#Program
+
+name="Arun"
+age=20
+marks=89.5
+
+print(name)
+print(age)
+print(marks)
