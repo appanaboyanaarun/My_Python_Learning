b9f6a2c2200a5ad6bd2f28e08576b203c5bd56e6
Arun <appanaboyanaarun@gmail.com>
2026-08-16 13:25:44 +0530

    Revise comments on variable usage in variables.py
    Updated comments to clarify variable usage in Python.

--- variables.py
+++ variables.py
@@ -1,9 +1,16 @@
 #In Python ,The Variables are used to store different values 
-#Python does not require you to specify the data type when creating a variable.
 #Without variable ,a program cannot store or process information
+#Python does not require you to specify the data type when creating a variable.
 ''' Example:
  age =20 #correct
  int age =20 #incorrect
+Imagine you have a Water Bottle 
+   Bottle is a varible 
+   water is a value 
+   You can fill the bottle with water .
+   Similarly , a Variable can store different values
+
+ 
  '''
 
 #Program
